5499facdb2f1979ac381869446ec6c4c7b3b54b5
149127807+luodeeu@users.noreply.github.com <149127807+luodeeu@users.noreply.github.com>
2025-04-08 16:06:42 +0800

    Localize response

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -9,6 +9,11 @@ module.exports = {
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		const locales = {
+			de: `Dieser Befehl wurde von ${interaction.user.username} ausgeführt, der/die am ${interaction.member.joinedAt} beigetreten ist.`,
+			'zh-CN': `这个命令是由 ${interaction.user.username} 执行的，他/她于 ${interaction.member.joinedAt} 加入。`
+		};
+
+		await interaction.reply(locales[interaction.locale] ?? `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
 	},
 };
