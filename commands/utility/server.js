dead2b87b6022a4666c621c5343af5f1df8b592f
149127807+luodeeu@users.noreply.github.com <149127807+luodeeu@users.noreply.github.com>
2025-04-08 15:50:04 +0800

    Fetch response message object

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -8,6 +8,8 @@ module.exports = {
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
 		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		const response = await interaction.reply({ content: `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`, withResponse: true });
+		
+		console.log(response.resource.message);
 	},
 };
