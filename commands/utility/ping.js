05fc60db7f44de2478c7435ada25598a5147409d
149127807+luodeeu@users.noreply.github.com <149127807+luodeeu@users.noreply.github.com>
2025-04-08 15:30:32 +0800

    Deferred response
    update discord version: npm i discord.js@latest
    bot is thinking ...

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	category: 'utility',
@@ -7,6 +8,8 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply({ content: 'Secret Pong!', flags: MessageFlags.Ephemeral });
+		await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+		await wait(5_000);
+		await interaction.editReply('Secret Pong!');
 	},
 };
